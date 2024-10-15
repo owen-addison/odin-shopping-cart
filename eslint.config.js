9e07fbf7645f7a5516f49634182941882bad3725
Owen Lewis <owenalewis@gmail.com>
2024-10-15 13:51:43 +0200

    [chore] Convert ESLint file

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import eslint from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'react': reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs['jsx-runtime'].rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/no-unknown-property': [
+        'error',
+        {
+          ignore: ['intensity', 'position', 'args'],
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: '18.2',
+      },
+    },
+  },
+  {
+    ignores: ['dist/**'],
+  },
+];
