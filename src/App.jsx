82247863b757de1b13c5516b5acbd6e23add27e4
Owen Lewis <owenalewis@gmail.com>
2024-10-21 12:51:10 +0200

    Add Cart route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { CartProvider } from './contexts/CartContext';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/shop" element={<Shop />} />
+              <Route path="/cart" element={<Cart />} />
             </Routes>
           </main>
           <footer className="bg-gray-200 text-center py-4">
