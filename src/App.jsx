64ce9676aa8ac4fa6e2209f8916817caffb696a3
Owen Lewis <owenalewis@gmail.com>
2024-10-21 12:45:42 +0200

    Integrate CartProvider in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './contexts/CartContext';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Nav />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-          </Routes>
-        </main>
-        <footer className="bg-gray-200 text-center py-4">
-          © 2024 Things. All rights reserved.
-        </footer>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Nav />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shop" element={<Shop />} />
+            </Routes>
+          </main>
+          <footer className="bg-gray-200 text-center py-4">
+            © 2024 Things. All rights reserved.
+          </footer>
+        </div>
+      </Router>
+    </CartProvider>
   );
 };
 
